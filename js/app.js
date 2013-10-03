500fbd5125d3ea87a83d271df81d4acca6a3b498
Jon Saints <saintsjd@gmail.com>
2013-10-03 16:24:01 -0600

    export buttons

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(function(){
+    'use strict';
+    
+    $('#support-link,#support-bar .close').click(function(){
+        $('#support-bar').slideToggle();
+    });
+
+    $('.filter-btn').click(function(){
+
+        var whichBtn = $(this).attr('id').split('-')[0];
+        var alreadyActive = $(this).hasClass('active');
+
+        $('.filter-btn').removeClass('active');
+        $('.filter').slideUp(function(){
+            console.log('up');
+        });
+
+        if( !alreadyActive ) {
+            $('#'+whichBtn+'-filter').slideToggle();
+            $(this).toggleClass('active');
+        }
+
+        return false;
+    });
+
+    $('.update-btn').click(function(){
+
+        $('.filter-btn').removeClass('active');
+        $('.filter').slideUp();
+        return false;
+    });
+
+
+    return '\'Allo \'Allo!';
+});
