ee8853955f7bc3663a771948f2d1becd622029c2
Jon Saints <saintsjd@gmail.com>
2013-10-22 23:21:48 -0600

    progress on clickable checkboxes for products

--- js/app.js
+++ js/app.js
@@ -48,28 +48,64 @@ $(function(){
         }
 
     });
-    products.forEach(function(m){
-        new ProductCheckBox({model:m});
-    });
 
+    var ProductLabel = Backbone.View.extend({
+        tagName: 'h4',
 
-    var ProductsSelectedDescription = Backbone.View.extend({
+        events: {
+        'click span.glyphicon-remove': 'remove',
+        },
 
         initialize: function() {
-            this.listenTo(this.collection, "change", this.render );
+            this.listenTo(this.model, "change selected", this.render );
+            this.$el.html(
+                '<span class="label label-primary">' + this.model.get('name') + ' &nbsp;&nbsp;<span class="glyphicon glyphicon-remove"></span></span>'
+                );
+            $("#products-selected-labels").append(this.$el);
             this.render();
         },
 
         render: function() {
+            //console.log(this.model.get('selected'));
+            if( this.model.get('selected') ) {
+                this.$el.fadeIn('slow');
+            }else{
+                console.log(this.$el);
+                this.$el.fadeOut('slow');
+            }
+        },
+
+        remove: function() {
+            this.model.set({selected:false});
+            this.$el.fadeOut("slow");
+        }
+
+    });
+
+
+    products.forEach(function(m){
+        new ProductCheckBox({model:m});
+        new ProductLabel({model:m});
+    });
+
+
+    var ProductsSelectedDescription = Backbone.View.extend({
+        initialize: function() {
+            this.listenTo(this.collection, "change", this.render );
+            this.render();
+        },
+        render: function() {
             var active = this.collection.where({selected: true});
             if( active.length == 0 ){
                 this.$el.html("None Selected");
+                $("#no-products-selected").fadeIn("slow");
             }else{
                 var toPrint = [];
                 _.forEach(active, function(i){
                     toPrint.push(i.get('name'));
                 });
                 this.$el.html(toPrint.join(', '));
+                $("#no-products-selected").hide();
             }
         },
     });
